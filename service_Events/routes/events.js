f4e2442ab3506251108c337a5429f1af0549b2f5
Skyrolight <skyrolight@gmail.com>
2023-03-28 15:39:39 +0200

    create + get all events

--- service_Events/routes/events.js
+++ service_Events/routes/events.js
@@ -13,11 +13,37 @@ const db = knex({
     }
 });  
 
-// Get all event
+// Get all events
 router.route('/')
     .get(async (req, res, next) => {
         try {
-            const result = await db('Event').select();
+            let result;
+            if (req.query.user) {
+                const events = await db('Event')
+                    .where('id_user' === req.query.user)
+                    .groupBy('id_event');
+                
+                const eventResult = events.map(event => {
+                    return {
+                        "event": {
+                            "id_event": event.id_event,
+                            "address": event.address,
+                            "state": event.state,
+                            "before": event.before,
+                            "after": event.after,
+                            "date_event": event.date_event
+                        }
+                    }
+                });
+
+                result = {
+                    "type": "collection",
+                    "count": eventResult.length,
+                    "events": eventResult
+                };
+            } else {
+                result = await db('Event').select();
+            }
 
             if (!result) {
                 res.status(404).json({
@@ -34,6 +60,26 @@ router.route('/')
                 "error": 500,
                 "message": "Erreur interne du serveur"
             });
+        }
+    });
+
+//create event
+router.route('/create')
+    .post(async (req, res, next) => {
+        try {
+            await db('Event').insert({
+                id_user: req.body.id_user,
+                address: req.body.address,
+                date_event: req.body.date_event,
+            });
+
+            res.status(201).json('event ajouté');
+        } catch (error) {
+            res.status(500).json({
+                "type": "error",
+                "error": 500,
+                "message": "Erreur interne du serveur"
+            })
         }
     });
 
